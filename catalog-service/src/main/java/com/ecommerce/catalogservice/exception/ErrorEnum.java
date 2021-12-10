ce5fc3eff4047aaa1a1cb5025081ce228aa57a0c
bum12ark <bum12ark@gmail.com>
2021-12-10 15:50:03 +0900

    전역 사용자 예외 처리 아키텍처 구현 - ControllerAdvice & ExceptionHandler

---
+++ catalog-service/src/main/java/com/ecommerce/catalogservice/exception/ErrorEnum.java
@@ -0,0 +1,16 @@
+package com.ecommerce.catalogservice.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter @AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorEnum {
+    NOT_EXIST_CATEGORY(HttpStatus.CONFLICT, "Not exist category"),
+    NOT_EXIST_CATALOG(HttpStatus.CONFLICT, "Not exist catalog");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
